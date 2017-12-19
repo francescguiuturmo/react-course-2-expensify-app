39403cbc6dad698246d9e0e7a9b9c60f41e581e4
francescguiuturmo <fguiu@opentrends.net>
2017-12-19 16:29:19 +0100

    Fetching expeneses from Firebase

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -18,6 +18,7 @@ export const ExpenseList = (props) => (
 );
 
 const mapStateToProps = (state) => {
+	console.log('list');
 	return {
 		expenses: selectExpenses(state.expenses, state.filters)
 	};
